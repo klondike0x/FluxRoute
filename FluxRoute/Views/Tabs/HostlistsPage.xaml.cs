259ef7badc4e15374349e406114cf0c1759de2dc
klondike0x <klondike0x@proton.me>
2026-08-21 21:31:48 +0300

    fix(ui): добавить диалог несохранённых изменений

--- FluxRoute/Views/Tabs/HostlistsPage.xaml.cs
+++ FluxRoute/Views/Tabs/HostlistsPage.xaml.cs
@@ -1,3 +1,4 @@
+using System.Windows;
 using FluxRoute.Services;
 using FluxRoute.ViewModels;
 using WpfUserControl = System.Windows.Controls.UserControl;
@@ -13,6 +14,29 @@ public partial class HostlistsPage : WpfUserControl
     public HostlistsPage()
     {
         InitializeComponent();
+        DataContextChanged += OnDataContextChanged;
+    }
+
+    private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+    {
+        if (e.NewValue is HostlistsViewModel vm)
+            vm.UnsavedChangesPrompt = PromptUnsavedChanges;
+    }
+
+    private static HostlistUnsavedChangesDecision PromptUnsavedChanges()
+    {
+        var result = MessageBox.Show(
+            "В редакторе есть несохранённые изменения. Что сделать?",
+            "Несохранённые изменения",
+            MessageBoxButton.YesNoCancel,
+            MessageBoxImage.Warning);
+
+        return result switch
+        {
+            MessageBoxResult.Yes => HostlistUnsavedChangesDecision.Save,
+            MessageBoxResult.No => HostlistUnsavedChangesDecision.Discard,
+            _ => HostlistUnsavedChangesDecision.Stay
+        };
     }
 
     /// <summary>
@@ -21,7 +45,10 @@ public partial class HostlistsPage : WpfUserControl
     public void Refresh()
     {
         if (DataContext is HostlistsViewModel vm)
+        {
+            vm.UnsavedChangesPrompt = PromptUnsavedChanges;
             vm.LoadHostlistFiles();
+        }
     }
 
     private void TabHelpButton_Click(object sender, System.Windows.RoutedEventArgs e)
